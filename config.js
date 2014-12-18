29ddf07c29fa8c43d0310598f9dc6e1792dd21ad
Sascha Droste <pid@posteo.net>
2014-12-18 17:12:49 +0100

    add sites

--- config.js
+++ config.js
@@ -75,119 +75,95 @@ module.exports = {
             defaultLimit: 3, // limit for rss latest
             timer: 3, // after 3 pings check feeds
             feeds: [
-                // echojs.com
                 'http://echojs.com/rss',
-                // heise.de
                 'http://www.heise.de/open/news/news-atom.xml',
                 'http://www.heise.de/security/news/news-atom.xml',
                 'http://www.techstage.de/rss.xml',
                 'http://www.heise.de/developer/rss/news-atom.xml',
-                // dailyjs.com
                 'http://feeds.feedburner.com/dailyjs?format=xml',
-                // t3n.de
                 'http://feeds.feedburner.com/aktuell/feeds/rss?format=xml',
-                // codegeekz.com
                 'http://feeds2.feedburner.com/codegeekz',
-                // perfectionkills.com/
                 'http://perfectionkills.com/feed.xml',
-                // golem.de
                 'http://rss.golem.de/rss.php?feed=RSS1.0',
-                // html5rocks.com
                 'http://feeds.feedburner.com/html5rocks',
-                // redit/javascript
                 'http://www.reddit.com/r/javascript/.rss',
-                //
                 'http://www.2ality.com/feeds/posts/default',
-                //
                 'https://hacks.mozilla.org/feed/',
-                //
                 'http://www.smashingmagazine.com/feed/',
-                //
                 'http://www.dezignmatterz.com/feed',
-                // http://highscalability.com/
                 'http://feeds.feedburner.com/HighScalability',
-                //
                 'http://www.silicon.de/feed/',
-                //
                 'http://www.drweb.de/magazin/feed/',
-                //
                 'http://feeds.feedburner.com/CssTricks',
-                // http://www.sitepoint.com/
                 'http://www.sitepoint.com/feed',
-                //
                 'http://ariya.ofilabs.com/feed',
-                //
                 'http://feeds.feedburner.com/ModernWebHQ',
-                //
-                'http://www.dezignmatterz.com/feed',
-                // http://tympanus.net/codrops/
                 'http://feeds2.feedburner.com/tympanus',
-                //
                 'http://news.centurylinklabs.com/rss',
-                //
                 'http://howtonode.org/feed.xml',
-                //
                 'http://marijnhaverbeke.nl/blog/feed.atom',
-                //
                 'http://www.geek-week.de/feed/',
-                //
                 'http://googlewebmastercentral.blogspot.com/atom.xml',
-                //
                 'http://www.seo-united.de/blog/feed',
-                //
                 'http://davidwalsh.name/feed',
-                //
                 'http://feeds.feedburner.com/TechCrunch/',
-                //
                 'http://ponyfoo.com/articles/feed',
-                //
                 'http://www.planetnodejs.com/feed',
-                //
                 'http://www.reddit.com/r/node/.rss',
-                //
                 'https://www.joyent.com/blog/feed',
-                //
                 'http://readwrite.com/rss.xml',
-                //
                 'http://www.bhorowitz.com/blog.rss',
-                //
                 'http://scotch.io/feed',
-                //
                 'http://www.onlinemarketingrockstars.de/feed/',
-                //
                 'http://www.netzaktiv.de/feed/',
-                //
                 'http://eisenbergeffect.bluespire.com/rss/',
-                //
                 'http://www.gizmodo.de/feed',
-                //
                 'http://javascriptweekly.com/rss/18em492a',
-                //
                 'http://mobilewebweekly.co/rss/1cd436ic',
-                //
                 'http://jsforallof.us/feed.xml',
-                //
                 'http://grochtdreis.de/weblog/feed/',
-                //,
                 'http://feeds.feedburner.com/atozcss',
-                //
                 'http://feeds.sidebar.io/SidebarFeed',
-                //
                 'http://blog.millermedeiros.com/feed/',
-                //
                 'http://www.merrickchristensen.com/atom.xml',
-                //
                 'http://www.heydonworks.com/feed',
-                //
                 'http://www.designmadeingermany.de/feed/',
-                //
                 'http://www.talentbuddy.co/blog/rss/',
-                //
                 'http://javascriptweekly.com/rss/2635acpd',
-                //
                 'http://angularjs.de/feed',
-                //
-                'http://blogs.msdn.com/b/eternalcoding/rss.aspx'
+                'http://blogs.msdn.com/b/eternalcoding/rss.aspx',
+                'http://feeds.feedburner.com/bocoup',
+                'http://feeds.feedburner.com/creativebloq/',
+                'http://shitformakingwebsites.com/feed/',
+                'http://feeds2.feedburner.com/webresourcesdepot',
+                'http://rss.badassjs.com/',
+                'http://substack.net/blog.xml',
+                'https://www.seroundtable.com/index.rdf',
+                'http://msdn.microsoft.com/de-de/magazine/cc194384.aspx',
+                'http://feeds.feedblitz.com/derickbailey',
+                'http://simpleprogrammer.com/feed/',
+                'http://joshuaearl.com/feed/',
+                'http://de.letsseewhatworks.com/feed/',
+                'http://programmingpraxis.com/feed/',
+                'http://xkcd.com/atom.xml',
+                'http://feeds.feedburner.com/codinghorror',
+                'http://feeds2.feedburner.com/thenextweb',
+                'http://www.webmonkey.com/feed/',
+                'http://feeds2.feedburner.com/Webappers',
+                'http://feeds.feedburner.com/webresourcesdepot?format=xml',
+                'http://ajaxian.com/index.xml',
+                'http://www.designfloat.com/rss.php',
+                'http://design.tutsplus.com/posts.atom',
+                'http://designreviver.com/feed/',
+                'http://feeds2.feedburner.com/SpoonGraphics',
+                'http://boxesandarrows.com/home/feed/',
+                'https://www.thebestdesigns.com/rss/feed.rss',
+                'http://alistapart.com/site/rss',
+                'http://webdesignfromscratch.com/category/html-css/feed/',
+                'http://www.456bereastreet.com/feed.xml',
+                'http://feeds2.feedburner.com/ProBlogDesign',
+                'http://calendar.perfplanet.com/feed/'
+
             ]
         }
     },
